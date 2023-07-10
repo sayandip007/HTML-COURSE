5dbcbcdbf6677b5f41b5f74b2a000df57804a51f
SAYANDIP SAHA <138747484+sayandip007@users.noreply.github.com>
2023-07-10 11:13:28 +0530

    Add files via upload

---
+++ avg.js
@@ -0,0 +1,7 @@
+var a = Number(prompt("Enter First Number: "));
+var b = Number(prompt("Enter Second Number: "));
+var c = Number(prompt("Enter Third Number: "));
+var sum=(a+b+c);
+var average = (sum) / 3;
+alert("Average is : "+average);
+console.log("The average of three numbers: " + average);
